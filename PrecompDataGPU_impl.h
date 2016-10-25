dd7b957b82132568bcc7b94eb1cb4f064091c579
pietrome <Pietro.Mele@insitu.com>
2016-10-25 14:53:19 +0100

    - Device, context, queue: create what is missing (i.e. null).

--- PrecompDataGPU_impl.h
+++ PrecompDataGPU_impl.h
@@ -40,24 +40,54 @@ int PrecompData<T>::CopyOnGPU(boost::compute::device         &device,
     return 0;
 }
 
-
+//+TEST
 template<typename T>
-int PrecompData<T>::CopyOnGPU(boost::compute::device         *device,
-                              boost::compute::context        *context,
-                              boost::compute::command_queue  *queue,
-                              boost::compute::vector<T>      *device_line)
 int PrecompData<T>::CopyOnGPU(boost::compute::device         **device,
                               boost::compute::context        **context,
                               boost::compute::command_queue  **queue,
                               boost::compute::vector<T>      **device_line)
 {
-    //+TODO - Create if null
-
     if(line.empty())
         return err_no_data;
+    
+    // Create what does not exist, only
 
+    if(*device == nullptr)
+    {
+        // New device
+        *device = compute::system::default_device();
+
+        // New device; previous context and command queue meaningless
+        (*context)->~context();
+        (*queue)->~command_queue();
+
+        // New context and command queue
+        *context = new compute::context(**device);
+        *queue   = new compute::command_queue(**context, **device);
+    }
+    else
+    {
+        if(*context == nullptr)
+        {
+            // New context; previous command queue meaningless
+            (*queue)->~command_queue();
+
+            // New context and command queue
+            *context = new compute::context(**device);
+            *queue   = new compute::command_queue(**context, **device);
+        }
+        else
+        {
+            if(*queue == nullptr)
+            {
+                // New command queue
+                *queue = new compute::command_queue(**context, **device);
+            }
+        }
+    }
+    
     // Resize vector on device
-    device_line->resize(line.size(), &context);
+    device_line->resize(line.size(), &queue);
 
     // Copy data from host to device
     boost::compute::copy(
