81fe454f70b0ae18b3ae02016a4f71ed57cdcae7
pietrome <Pietro.Mele@insitu.com>
2016-10-26 11:48:11 +0100

    - Test 1: doubled the number of test points, including some not used for the initialization.

--- PrecompData_test.cpp
+++ PrecompData_test.cpp
@@ -21,12 +21,13 @@ int main()
 	using namespace Utilities;
 
 	{ // Test 1
+        cout << "\n\nTest 1: Zero-degree (nearest-neighbor/point sampling/Voronoi) interpolation:" << endl;
 		const string funcName = "TestFunc";
 		PrecompData<float> itp(funcName);
 		itp.SetComment("TestFunc approximation");
 		const float x0 = 0.0f, x1 = 6.28f;
 		const int nValues = 10;
-		const float step = (x1 - x0)/nValues;
+		const float step = 0.5*(x1 - x0)/nValues;
 		itp.Set(&TestFunc, x0, x1, nValues);
 		float x = x0;
 		for(int i = 0; i < nValues; ++i) {
